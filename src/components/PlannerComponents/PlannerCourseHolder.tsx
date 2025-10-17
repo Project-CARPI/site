4535ddfe73be93f87317088be6719f330b325bd0
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-17 17:25:39 -0400

    clean up plannerCourseHolder

--- src/components/PlannerComponents/PlannerCourseHolder.tsx
+++ src/components/PlannerComponents/PlannerCourseHolder.tsx
@@ -8,21 +8,15 @@ const PlannerCourseHolder: React.FC<PlannerCourseHolderProps> = ({
   isHover,
 }) => {
   return (
-    <>
-      <div
-        className={`relative h-18 w-full border-[#c3a9a9] border-dashed border-2 rounded-md text-[#444444] text-center p-5 font-['Helvetica'] bg-transparent transition-all duration-200 ease-in-out`}
-      >
+    <div className="relative h-18 w-full border-[#c3a9a9] border-dashed border-2 rounded-md text-[#444444] text-center p-5 bg-transparent transition-all duration-200 ease-in-out">
+      {isHover ? (
+        <p className="text-[#283044] font-semibold">Drop it here!</p>
+      ) : (
         <p className="opacity-100 pointer-events-none">
           Try dragging a course from your Toolbox
         </p>
-
-        {isHover && (
-          <div className="absolute top-0 left-0 w-full h-full bg-white rounded-md z-10 shadow-xl flex items-center justify-center">
-            <p className="text-[#283044] font-semibold">Drop it here!</p>
-          </div>
-        )}
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 

0e513c8c2ef8cebc59449ad5bfd0bc22ca66bbf6
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-21 17:20:11 -0400

    add padding to course place holder

--- src/components/PlannerComponents/PlannerCourseHolder.tsx
+++ src/components/PlannerComponents/PlannerCourseHolder.tsx
@@ -9,7 +9,7 @@ const PlannerCourseHolder: React.FC<PlannerCourseHolderProps> = ({
 }) => {
   return (
     <div
-      className={`relative h-20 flex justify-center items-center w-full border-[#c3a9a9] border-dashed border-2 rounded-xl transition-all duration-200 ease-in-out ${isHover ? "bg-darkblue/20" : ""}`}
+      className={`h-20 p-4 flex justify-center items-center w-full border-[#c3a9a9] border-dashed border-2 rounded-xl transition-all duration-200 ease-in-out ${isHover ? "bg-darkblue/20" : ""}`}
     >
       {isHover ? (
         <p>Drop the course!</p>

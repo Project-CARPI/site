d97c90bd7e096e017ea83d6cfe401a66898505cb
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-10-24 16:39:33 -0400

    add close on click outside for filter popup

--- src/components/SearchBar/SeachBar.tsx
+++ src/components/SearchBar/SeachBar.tsx
@@ -24,6 +24,7 @@ const SearchBar: React.FC<SearchBarProps> = ({
   semesters,
 }) => {
   const isDesktop = useIsDesktop();
+  const componentRef = useRef<HTMLDivElement>(null);
 
   const [showDeptFilter, setShowDeptFilter] = useState(true);
   const [showFilter, setShowFilter] = useState(false);
@@ -146,9 +147,28 @@ const SearchBar: React.FC<SearchBarProps> = ({
     return Object.values(filters).flat();
   }, [filters]);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        componentRef.current &&
+        !componentRef.current.contains(event.target as Node)
+      ) {
+        setShowFilter(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="flex justify-between flex-col items-center gap-2">
-      <div className="flex items-center gap-4 w-full border-b boarder-darkblue p-2">
+      <div
+        ref={componentRef}
+        className="flex items-center gap-4 w-full border-b boarder-darkblue p-2"
+      >
         <IoSearchOutline className="w-5 h-5" />
         <input
           type="text"

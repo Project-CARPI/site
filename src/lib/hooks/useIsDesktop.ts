4750dd4fbd793a60a2d76aec1410d4bc4a14121c
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-02-10 17:18:09 -0500

    reduce desktop breakpoint

--- src/lib/hooks/useIsDesktop.ts
+++ src/lib/hooks/useIsDesktop.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const DESKTOP_BREAKPOINT = 1024;
+const DESKTOP_BREAKPOINT = 768;
 const useIsDesktop = () => {
   const [isDesktop, setIsDesktop] = useState(
     window.innerWidth > DESKTOP_BREAKPOINT,

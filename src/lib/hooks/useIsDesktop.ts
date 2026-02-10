f69bffe338123af048e36a5c9000d20a33754bd0
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-02-10 17:25:52 -0500

    Update useIsDesktop.ts

--- src/lib/hooks/useIsDesktop.ts
+++ src/lib/hooks/useIsDesktop.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const DESKTOP_BREAKPOINT = 768;
 const useIsDesktop = () => {
   const [isDesktop, setIsDesktop] = useState(
-    window.innerWidth > DESKTOP_BREAKPOINT,
+    window.innerWidth >= DESKTOP_BREAKPOINT,
   );
 
   useEffect(() => {

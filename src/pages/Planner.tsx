5be00cd4e4640188c8b0ef22ee6fa74cd5d384c8
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-02-15 16:04:34 -0500

    fix mobile planner height

--- src/pages/Planner.tsx
+++ src/pages/Planner.tsx
@@ -22,7 +22,7 @@ const Planner: React.FC = () => {
   const { plannerCourses } = useCourseWorkspace();
 
   return (
-    <div className="md:h-[calc(100vh-10rem)] md:m-0 m-4 h-screen">
+    <div className="md:h-[calc(100vh-10rem)] md:m-0 m-4">
       <section className="h-full w-full flex flex-col gap-4">
         <div className="flex justify-between sticky top-0 z-10 bg-carpipink pt-4">
           <h1 className="font-bold text-xl">Planner</h1>

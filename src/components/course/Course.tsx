1c3946e0fcabf0421ef5aa07dec0a9063ee14720
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-04-07 14:44:28 -0400

    Update Course.tsx

--- src/components/course/Course.tsx
+++ src/components/course/Course.tsx
@@ -12,6 +12,7 @@ import CourseMenuContent from "@/features/planner/components/course/CourseMenuCo
 import CreditSelector from "@/features/planner/components/CreditSelector";
 import { usePlannerCourse } from "@/features/planner/usePlannerCourse";
 import { cn } from "@/lib/classnames";
+import useIsDesktop from "@/lib/hooks/useIsDesktop";
 import { UserCourse } from "@/lib/types";
 
 type CourseVariant = "toolbox" | "planner";
@@ -95,6 +96,8 @@ function PlannerCourseView({
     course,
     semesterId: semesterId ?? null,
   });
+
+  const isDesktop = useIsDesktop();
   const [popoverOpen, setPopoverOpen] = useState(false);
 
   if (!semesterId) {
@@ -109,7 +112,7 @@ function PlannerCourseView({
 
   return (
     <ContextMenu.Root>
-      <ContextMenu.Trigger>
+      <ContextMenu.Trigger disabled={!isDesktop}>
         <div
           ref={innerRef}
           className={cn(

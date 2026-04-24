efa4cf2f7b66da18704e3b7a40402103c4f36bc1
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2026-04-24 09:48:24 -0400

    fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ButtonTray from "@/components/header/ButtonTray";
-import SocialTray from "@/components/header/SocialTray";
 import { CourseWorkspaceProvider } from "@/core/workspace/provider";
 import WorkspaceDndProvider from "@/features/dnd/dnd.tsx";
 import Toolbox from "@/features/toolbox/Toolbox";
